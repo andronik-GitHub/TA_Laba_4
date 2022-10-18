84545b34847770d7095c9b99abd1b608c52e7096
kazmi <costellationoblivion@gmail.com>
2022-10-18 20:25:43 +0300

    Добавьте файлы проекта.

---
+++ Laba_4/DirectMergeSort.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Drawing;
+using System.Reflection;
+using System.Text;
+
+class DirectMergeSort<T> where T : IComparable<T>
+{
+    static void Merge(T[] array, int lowIndex, int middleIndex, int highIndex)
+    {
+        var left = lowIndex;
+        var right = middleIndex + 1;
+        var tempArray = new T[highIndex - lowIndex + 1];
+        var index = 0;
+
+        while ((left <= middleIndex) && (right <= highIndex))
+        {
+            if (array[left].CompareTo(array[right]) < 0)
+            {
+                tempArray[index] = array[left];
+                left++;
+            }
+            else
+            {
+                tempArray[index] = array[right];
+                right++;
+            }
+
+            index++;
+        }
+
+        for (var i = left; i <= middleIndex; i++)
+        {
+            tempArray[index] = array[i];
+            index++;
+        }
+
+        for (var i = right; i <= highIndex; i++)
+        {
+            tempArray[index] = array[i];
+            index++;
+        }
+
+        for (var i = 0; i < tempArray.Length; i++)
+            array[lowIndex + i] = tempArray[i];
+    }
+
+    static T[] MergeSort(T[] array, int lowIndex, int highIndex)
+    {
+        if (lowIndex < highIndex)
+        {
+            var middleIndex = (lowIndex + highIndex) / 2;
+            MergeSort(array, lowIndex, middleIndex);
+            MergeSort(array, middleIndex + 1, highIndex);
+            Merge(array, lowIndex, middleIndex, highIndex);
+        }
+
+        return array;
+    }
+
+    public static T[] MergeSort(T[] array)
+    {
+        return MergeSort(array, 0, array.Length - 1);
+    }
+}
---
+++ Laba_4/Laba_4.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Laba_4
+{
+    static void Main()
+    {
+        int N = Convert.ToInt32(Console.ReadLine());
+        int[] intArr = new int[N];
+        Random r = new();
+        for (int i = 0; i < N; i++) intArr[i] = r.Next(-100, 100);
+
+        Console.WriteLine(string.Join(" ", intArr) + "\n");
+        DirectMergeSort<int>.MergeSort(intArr);
+        Console.WriteLine(string.Join(" ", intArr));
+
+
+
+        char[] arr;
+        string temp;
+
+        temp = Console.ReadLine();
+        temp = "afdfabcabcadefdea";
+
+        arr = temp.ToCharArray();
+
+        Console.WriteLine("\n\n\n\n" + string.Join("", arr));
+        DirectMergeSort<char>.MergeSort(arr);
+        Console.WriteLine(string.Join("", arr));
+
+
+
+        Console.ReadKey();
+    }
+    
+}
